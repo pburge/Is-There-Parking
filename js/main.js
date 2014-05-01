ac13818e8ca70d9948b38b87c982494b1a1abc8b
Paul Burge <paul.burge8@gmail.com>
2014-05-01 17:57:52 -0400

    added angularjs, added test markers and current position

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-function initialize() {
-	var lat = 28.5968780;
-	var lng = -81.3007476;
+// function initialize() {
+// 	var lat = 28.5968780;
+// 	var lng = -81.3007476;
 
-	var pos = new google.maps.LatLng(lat,lng);
+// 	var pos = new google.maps.LatLng(lat,lng);
 
-	var mapOptions = {
-		zoom: 19,
-		center: new google.maps.LatLng(lat,lng),
-		mapTypeId: google.maps.MapTypeId.SATELLITE
-	};
+// 	var mapOptions = {
+// 		zoom: 19,
+// 		center: new google.maps.LatLng(lat,lng),
+// 		mapTypeId: google.maps.MapTypeId.SATELLITE
+// 	};
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-	var myMarker = new google.maps.Marker({ position: pos, map: map, title:"About.com Headquarters" });
-}
+// 	var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+// 	var myMarker = new google.maps.Marker({ position: pos, map: map, title:"Open Parking Spot" });
+// }
+
+// google.maps.event.addDomListener(window, "load", initialize);
+
+
